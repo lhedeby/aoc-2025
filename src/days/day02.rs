433509bccfe52b20c7b0397b4490178fae3f535c
Ludwig <ludwig@hedeby.me>
2025-12-03 22:25:20 +0100

    maybe faster

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -17,16 +17,23 @@ pub fn solve() -> Result<(), Box<dyn Error>> {
         let (l, r) = r.split_once("-").expect("will always have '-'");
         let n1: usize = l.parse()?;
         let n2: usize = r.parse()?;
-        for i in n1..n2 + 1 {
-            let s = i.to_string();
-            if let Some(_) = find_subs(&s) {
-                p2 += i;
-            }
-            let (l, r) = s.split_at(s.len() / 2);
+        let mut temp: Vec<u8> = r.chars().map(|c| c.to_digit(10).unwrap() as u8).collect();
 
-            if l == r {
-                p1 += i;
+        for _ in n1..n2 {
+            if let Some(a) = find_repeats(&temp, true) {
+                p1 += a;
             }
+            if let Some(a) = find_repeats(&temp, false) {
+                p2 += a;
+            }
+            dec_vec(&mut temp, 0);
+        }
+
+        if let Some(a) = find_repeats(&temp, true) {
+            p1 += a;
+        }
+        if let Some(a) = find_repeats(&temp, false) {
+            p2 += a;
         }
     }
 
@@ -35,23 +42,49 @@ pub fn solve() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn find_subs(s: &str) -> Option<()> {
-    let len = s.len();
-    for j in 1..(len / 2) + 1 {
-        let sub = &s[0..j];
-        if len % sub.len() != 0 {
+fn dec_vec(v: &mut Vec<u8>, n: usize) {
+    let p = v.len() - 1 - n;
+    if v[p] == 0 {
+        v[p] = 9;
+        dec_vec(v, n + 1)
+    } else {
+        v[p] -= 1;
+    }
+}
+
+fn find_repeats(v: &Vec<u8>, is_p1: bool) -> Option<usize> {
+    let mut start = 0;
+    let mut len = v.len();
+    while v[start] == 0 {
+        start += 1;
+        len -= 1;
+    }
+    let max_len = (len) / 2;
+    for l in 1..(max_len + 1) {
+        if len % l != 0 {
             continue;
         }
-        let count = len / sub.len();
+        let mut has_repeat = true;
 
-        let mut sub_count = 0;
-        for c in 0..count {
-            if *sub == s[c * sub.len()..c * sub.len() + sub.len()] {
-                sub_count += 1;
+        for i in 0..l {
+            let expected = v[start + i];
+            let groups = len / l;
+            for j in 1..groups {
+                if v[start + i + j * l] != expected {
+                    has_repeat = false;
+                }
             }
         }
-        if sub_count == count {
-            return Some(());
+        if has_repeat {
+            if !is_p1 || (len % 2 == 0 && l == len / 2) {
+                let mut res: usize = 0;
+                for r in start..(len + start) {
+                    res += v[r] as usize;
+                    res *= 10;
+                }
+                res /= 10;
+                return Some(res);
+            }
         }
     }
     None
