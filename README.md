50866233a4f086df6fa40314d1c1054a7d3273fe
Ludwig <ludwig@hedeby.me>
2025-12-07 21:22:26 +0100

    optimize day02

--- README.md
+++ README.md
@@ -10,14 +10,18 @@ Solve all problems in less than 1s total.
 
 ### Unreasonable goal progress
 ```
-Day 1: p1: 1055, p2: 6386 (time: 669.358µs)
-Day 2: p1: 32976912643, p2: 54446379122  (time: 111.177ms)
-Day 3: p1: 17430, p2: 171975854269367  (time: 432.247µs)
-Day 4: p1: 1393, p2: 8643  (time: 2.520ms)
-Day 5: p1: 726, p2: 354226555270043  (time: 1.705ms)
-Total time: 116.550ms
+Day 1: p1: 1055, p2: 6386 (time: 691.150µs)
+Day 2: p1: 32976912643, p2: 54446379122  (time: 20.597ms)
+Day 3: p1: 17430, p2: 171975854269367  (time: 322.178µs)
+Day 4: p1: 1393, p2: 8643  (time: 1.945ms)
+Day 5: p1: 726, p2: 354226555270043  (time: 1.074ms)
+Day 6: p1: 6371789547734, p2: 11419862653216  (time: 89.327µs)
+Day 7: p1: 1504, p2: 5137133207830  (time: 75.429µs)
+Total time: 24.834ms
 ```
 ### Hardware
 AMD Ryzen 5 5600H
 16GB ram
 
+### Btw
+Only 1 thread
--- src/days/day02.rs
+++ src/days/day02.rs
@@ -4,7 +4,6 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-
 pub fn solve(path: &str) -> Result<(usize, usize), Box<dyn Error>> {
     let f = File::open(path)?;
     let reader = BufReader::new(f);
@@ -21,20 +20,20 @@ pub fn solve(path: &str) -> Result<(usize, usize), Box<dyn Error>> {
         let mut temp: Vec<u8> = r.chars().map(|c| c.to_digit(10).unwrap() as u8).collect();
 
         for _ in n1..n2 {
-            if let Some(a) = find_repeats(&temp, true) {
-                p1 += a;
-            }
-            if let Some(a) = find_repeats(&temp, false) {
-                p2 += a;
+            if let Some(a) = find_repeats(&temp) {
+                if a.1 {
+                    p1 += a.0;
+                }
+                p2 += a.0;
             }
             dec_vec(&mut temp, 0);
         }
 
-        if let Some(a) = find_repeats(&temp, true) {
-            p1 += a;
-        }
-        if let Some(a) = find_repeats(&temp, false) {
-            p2 += a;
+        if let Some(a) = find_repeats(&temp) {
+            if a.1 {
+                p1 += a.0;
+            }
+            p2 += a.0;
         }
     }
 
@@ -53,41 +52,46 @@ fn dec_vec(v: &mut Vec<u8>, n: usize) {
     }
 }
 
-fn find_repeats(v: &Vec<u8>, is_p1: bool) -> Option<usize> {
+fn find_repeats(v: &Vec<u8>) -> Option<(usize, bool)> {
     let mut start = 0;
     let mut len = v.len();
+
     while v[start] == 0 {
         start += 1;
         len -= 1;
     }
-    let max_len = (len) / 2;
-    for l in 1..(max_len + 1) {
+    let max_len = len / 2;
+    for l in (1..(max_len + 1)).rev() {
         if len % l != 0 {
             continue;
         }
         let mut has_repeat = true;
 
-        for i in 0..l {
+        'outer: for i in 0..l {
             let expected = v[start + i];
             let groups = len / l;
             for j in 1..groups {
                 if v[start + i + j * l] != expected {
                     has_repeat = false;
+                    break 'outer;
                 }
             }
         }
+
         if has_repeat {
-            if !is_p1 || (len % 2 == 0 && l == len / 2) {
-                let mut res: usize = 0;
-                for r in start..(len + start) {
-                    res += v[r] as usize;
-                    res *= 10;
-                }
-                res /= 10;
-                return Some(res);
+            let mut res: usize = 0;
+            for r in start..(len + start) {
+                res *= 10;
+                res += v[r] as usize;
             }
+            let is_p1 = if len % 2 == 0 && l == len / 2 {
+                true
+            } else {
+                false
+            };
+
+            return Some((res, is_p1));
         }
     }
     None
 }
-
